efce5e060fe79ed2b615c175db613d30b6f39aa6
JaseSmoot <jase.smoot@purpleknights.com>
2021-07-01 15:24:17 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
          function drawCircle (){// Code to draw a circle
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle , canvas, 20, 20);
             view.addChild(circle);
             circles.push(circle);
             
@@ -63,11 +63,11 @@ var init = function (window) {
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(  );
-            game.checkCirclePosition(  );
-            game.checkCirclePosition(  );
-            game.checkCirclePosition(  );
-            game.checkCirclePosition(  );
+            game.checkCirclePosition( circles[0] );
+            game.checkCirclePosition( circles[1] );
+            game.checkCirclePosition( circles[2] );
+            game.checkCirclePosition( circles[3] );
+            game.checkCirclePosition( circles[4] );
 
 
             // TODO 9 : Iterate over the array
